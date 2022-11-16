a79313d2ab0ddad6eae39bfb9ad5ff6fbd9b2afa
Matthew Stoodley <matt.storyfeet@gmail.com>
2022-11-16 11:39:32 +0000

    creating VP Logo so we can add that to the rules

--- src/Campaign.elm
+++ src/Campaign.elm
@@ -4,9 +4,10 @@ import Job exposing (Job)
 type alias Campaign =
     { name : String 
     , difficulty : Int
+    , dice : Dice
     , mode : Mode
     , board : String
-    , setup : Maybe String
+    , setup : String
     , rules : String
     , jobs : List Job
     , scoring : List Score
@@ -23,6 +24,34 @@ type Score
     | Reveal Int Int
     | WagonWest Int
     | WagonEast Int
+    | BarWest Int
     | RemoveBandits Int
+
+type Dice
+    = D8
+    | D10
+    | D12
+    | D20
+
+
+verses1 : Campaign
+verses1 = { name = "Verses 1"
+    , difficulty = 1
+    , dice = D20
+    , mode = Verses
+    , board = "Verses Short"
+    , setup = ""
+    , rules = ""
+    , jobs = []
+    , scoring = [Build 2 3,Reveal 1 2, RemoveBandits 2]
+    }
+
+coop1 : Campaign
+coop1 = {verses1 
+    | name = "Precious Cargo"
+    , mode = Coop
+    , board = "Co-op Short"
+    , scoring = [Build 2 3, WagonWest 2,RemoveBandits 2 ]
+    }
 
 
