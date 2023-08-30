16581af0ed87765e129cdbd9b6b2008fbf7d5fe6
Matthew Stoodley <matt.storyfeet@gmail.com>
2023-08-30 12:59:33 +0100

    land tiles now have movement indicators

--- src/TileSvg.elm
+++ src/TileSvg.elm
@@ -5,9 +5,51 @@ import Job exposing (Job)
 import JobSvg as JSV
 import Config 
 
+type CanMove 
+    = BoatFromWater
+    | Boat
+    | Walk
+    | Walk2
+    | Climb
+
+miniPic : Float -> Float -> String -> String
+miniPic x y fname =
+    img x y 5 5 ("../pics/jobs/" ++ fname  ++ ".svg")[]
+
+movRect: Float -> Float -> Float -> String
+movRect x y w =
+    rect (x - 1.4) y (w * 6 + 2) 5 [flNoStk "white",opacity 0.4]
+
+drawMovement : Float -> Float -> CanMove -> String
+drawMovement x y m =
+    case m of
+        BoatFromWater -> (movRect x y 2 ) ++ (miniPic x y "from_water")  ++ (miniPic (x + 6) y "sail" ) ++ (miniPic (x + 3) y "from_arrow" )
+        Boat -> (movRect x y 1) ++ miniPic x y "sail"
+        Walk -> (movRect x y 1) ++ miniPic x y "move"
+        Walk2 -> (movRect x y 2) ++ (miniPic x y "move") ++ (miniPic (x + 6) y "move" )
+        Climb -> (movRect x y 1) ++ miniPic x y "climb"
+
+
+
+tileMovement : LType -> List CanMove
+tileMovement t =
+    case t of
+        Water -> [Boat]
+        Mountain -> [Walk2,Climb]
+        Forest True -> [Boat,Climb,Walk]
+        Prairie True -> [Boat,Climb,Walk]
+        _ -> [BoatFromWater,Climb,Walk]
+
+movements : Float -> Float -> List CanMove -> String
+movements x y l =
+    case l of
+        [] -> ""
+        h::t -> drawMovement x y h ++ movements x (y - 6) t
+
 wet : Bool -> String
 wet b = 
     if b then "_wet" else ""
+
 
 
 tileName : LType -> String
@@ -38,6 +80,7 @@ front t =
     [ img 0 0 45 45  (pLink t.ltype) []
 --    , rect 0 0 45 45 [flNoStk "white", opacity 0.5]
     , tileJob t
+    , tileMovement t.ltype |> movements 2 38
     ]
 
 back : Tile -> String
@@ -63,10 +106,10 @@ banditPos : Int -> Int -> (Float, Float)
 banditPos n from = 
     case (n , from )of
         (_,1) -> (17.5,17.5)
-        (0,_) -> (32,15)
-        (1,_) -> (3,15)
-        (2,_) -> (10,3)
-        (3,_) -> (25,3)
+        (0,_) -> (34,18)
+        (1,_) -> (3,12)
+        (2,_) -> (12,3)
+        (3,_) -> (26,5)
         (v,_) -> (toFloat v , 15)
 
 placeBandits : Int -> Int -> List Int -> List String
@@ -98,7 +141,8 @@ bStar x y n = String.join "\n"
 job: Job -> String
 job j = 
     let 
-        x = ( 45 - (JSV.jobLen j)) * 0.5
+        -- x = ( 45 - (JSV.jobLen j)) * 0.5
+        x = ( 40 - (JSV.jobLen j))
         
     in
          JSV.job 41 x 32 j
